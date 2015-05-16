20aae2897aca798370d61efc997c0d6c48a8cf1f
南山 <junjiemars@gmail.com>
2015-05-16 22:02:47 +0800

    uninstall jdk on mac os x

---
+++ mac/uninstall-jdk.sh
@@ -0,0 +1,5 @@
+JDK_VER=jdk1.8.0_40.jdk
+
+rm -rf /Library/Java/JavaVirtualMachines/$JDK_VER
+rm -rf /Library/PreferencePanes/JavaControlPanel.prefPane
+rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
