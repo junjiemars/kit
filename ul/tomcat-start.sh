0c7122e9841650d706e7d5e4c3a94bc465f7c43a
南山 <junjiemars@gmail.com>
2015-07-15 00:25:08 +0800

    merge HEAP_DUMP within DEBUG and add CATALINA_OPTS, but it's not work

--- ul/tomcat-start.sh
+++ ul/tomcat-start.sh
@@ -4,41 +4,36 @@ RUN=${RUN:="stop"}
 VER="${VER:=7062}"
 DEBUG="${DEBUG:=0}"
 MODE="NORMAL"
-DUMP="${DUMP:=0}"
-HEAP_DUMP=""
+CATALINA_OPTS="${CATALINA_OPTS}"
 JPDA=""
 HTTP_PORT=${HTTP_PORT:=8080}
 STOP_PORT=${STOP_PORT:=8005}
-BASE=${BASE:="/opt/tomcat"}
+BASE=${BASE:="/opt/bin/tomcat"}
 
 export CATALINA_BASE=${CATALINA_BASE:="${BASE}/${VER}"}
-
-if [ "$DUMP" -gt 0 ]; then
-    HEAP_DUMP="-XX:+HeapDumpOnOutOfMemoryError  \
-        -XX:HeapDumpPath=${CATALINA_BASE}/logs"
-    HEAP_DUMP=$(echo $HEAP_DUMP | tr -s " ")
-fi
 
 if [ "$DEBUG" -gt 0 ]; then
     MODE="DEBUG"
     JPDA=jpda
+    CATALINA_OPTS="${CATALINA_OPTS}             \
+        -XX:+HeapDumpOnOutOfMemoryError         \
+        -XX:HeapDumpPath=${CATALINA_BASE}/logs"
 fi
+CATALINA_OPTS=$(echo $CATALINA_OPTS | tr -s " ")
 
 JAVA_OPTS="${JAVA_OPTS}                      \
     -Dhttp.port=${HTTP_PORT}                 \
     -Dshutdown.port=${STOP_PORT}             \
-    -Dcom.sun.management.jmxremote           \
-    ${HEAP_DUMP} "
+    -Dcom.sun.management.jmxremote"
 JAVA_OPTS=$(echo $JAVA_OPTS | tr -s " ")
-
 BIN="$CATALINA_BASE/bin/catalina.sh $JPDA"
 
 if [ "$#" -gt 0 ];then
     RUN=$1
 fi
-case $RUN in
-    "stop") $BIN stop ;;
-    "start") $BIN start ;;
+case ".$RUN" in
+    ".stop") $BIN stop ;;
+    ".start") $BIN start ;;
     *) echo "#unknown command:$RUN" ;;
 esac
 
