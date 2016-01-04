f98e7aea67d0efc23ebb8e2cb28a104ede1dbe4f
南山竹 <junjiemars@gmail.com>
2016-01-04 11:25:51 +0800

    add NLS_LANG for sqlplus.sh

--- ul/oracle/sqlplus.sh
+++ ul/oracle/sqlplus.sh
@@ -1,6 +1,6 @@
 # sqlplus wrapper
-SQLPATH=${SQLPATH:-"$HOME/apps/kit/ul/oracle"}
-export SQLPATH
+export SQLPATH=${SQLPATH:-"$HOME/apps/kit/ul/oracle"}
+export NLS_LANG=AMERICAN_AMERICA.UTF8
 
 if [ $# -eq 0 ] ; then
 	rlwrap sqlplus system/password@host:1521/XE
