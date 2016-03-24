210b04169a558d6078704d68cdc89ef8df6f52d4
junjiemars <junjiemars@gmail.com>
2016-03-24 18:06:30 +0800

    refine sqlplu.sh

--- ul/oracle/sqlplus.sh
+++ ul/oracle/sqlplus.sh
@@ -1,13 +1,17 @@
-# sqlplus wrapper
-export SQLPATH=${SQLPATH:-"$HOME/apps/kit/ul/oracle"}
+#!/bin/bash
+
+# 1. download instantclient basic and unzip it;
+# 2. download sqlplus and unzip into basic;
+
+export SQLPLUS_HOME=${SQLPLUS_HOME:-"/opt/oracle/instantclient_12_1/"}
+export SQLPATH=${SQLPATH:-"/opt/oracle/sql"}
 export NLS_LANG=AMERICAN_AMERICA.UTF8
+export LD_LIBRARY_PATH=$SQLPLUS_HOME${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export PATH=$SQLPLUS_HOME:$PATH
 
 if [ $# -eq 0 ] ; then
 	rlwrap sqlplus system/password@host:1521/XE
 else 
 	rlwrap sqlplus $@
 fi
-
-# remote:
-# sqlplus sms_search/sms123@10.32.65.238:1521/orcl
 
