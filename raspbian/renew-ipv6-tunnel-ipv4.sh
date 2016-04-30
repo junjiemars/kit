845d3a12b8afefee549447a4f5f45f64a0b3bfbb
南山竹 <junjiemars@gmail.com>
2016-04-30 14:02:05 +0800

    replace the testing -eq

--- raspbian/renew-ipv6-tunnel-ipv4.sh
+++ raspbian/renew-ipv6-tunnel-ipv4.sh
@@ -5,7 +5,7 @@ IPV4_ISP=${IPV4_ISP:-"https://api.ipify.org?format=json"}
 SLEEP_S=10s
 #`logger -p local3.info -t $(basename $0)[$$] $(curl -s4 ${IPV6_TUNNEL})`
 
-if [[ 0 -eq `ping6 -q -c3 ${IPV6_TEST} 2>&1 >/dev/null;echo $?` ]]; then
+if [[ 0 -lt `ping6 -q -c3 ${IPV6_TEST} 2>&1 >/dev/null;echo $?` ]]; then
     for i in $(seq 1 10); do
         echo $i
         if [[ 0 -eq `curl -s4 ${IPV6_TUNNEL} 2>&1 >/dev/null;echo $?` ]]; then
