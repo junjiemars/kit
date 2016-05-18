e572c02a089edeaa6fb56400778c79a8bf997e34
南山竹 <junjiemars@gmail.com>
2016-05-19 00:52:42 +0800

    refine renew ipv6 tunnel

--- raspbian/renew-ipv6-tunnel-ipv4.sh
+++ raspbian/renew-ipv6-tunnel-ipv4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 IPV6_TEST=${IPV6_TEST:-"ipv6-test.com"}
+IPV6_KEY='<ipv6-key>'
+IPV6_SN='<ipv6-sn>'
 IPV6_TUNNEL=${IPV6_TUNNEL:-"<ipv6-tunnel>"}
 IPV6_STAT=${IPV6_TUNNEL_STAT:-/tmp/ipv6.stat}
 IPV4_ISP=${IPV4_ISP:-"https://api.ipify.org?format=json"}
@@ -13,6 +15,8 @@ HOST_PASS=${HOST_PASS:-"<host-passwd>"}
 DNS_RENEW=${DNS_RENEW:-"http://dyn.dns.he.net/nic/update"}
 SSH_LOGGER=${SSH_LOGGER:-"<ssh-logger>"}
 SLEEP_S=10s
+
+DEBUG=${DEBUG:-0}
 
 get_ip_addr() {
     local _addr=$1
@@ -53,29 +57,34 @@ ipv4_rot() {
     [ ".${_d4}" = ".${_r4}" ]; echo $?
 }
 
-if [ "0" != `ping6_check ${IPV6_TEST}` ] || [ "0" != `ipv4_rot` ]; then
-    for i in $(seq 1 10); do
-        if [[ 0 -eq `curl -s4 ${IPV6_TUNNEL} 2>&1 >${IPV6_STAT};echo $?` ]]; then
-            echo $(basename $0)[$$] ipv6 tunnel is ok
-            if [[ 0 -eq `curl -s4 ${IP_CHECK} 2>&1 >${IPV4_FILE};echo $?` ]]; then
-                IPV4_ADDR="$(get_ip_addr ${IPV4_FILE})"
-                post_ip_addr ${IPV4_ADDR} 4
+if [[ 0 -eq ${DEBUG} ]]; then
+    if [ "0" != `ping6_check ${IPV6_TEST}` ] || [ "0" != `ipv4_rot` ]; then
+        for i in $(seq 1 10); do
+            if [[ 0 -eq `curl -s4 ${IPV6_TUNNEL} 2>&1 >${IPV6_STAT};echo $?` ]]; then
+                echo $(basename $0)[$$] ipv6 tunnel is ok
+                if [[ 0 -eq `curl -s4 ${IP_CHECK} 2>&1 >${IPV4_FILE};echo $?` ]]; then
+                    IPV4_ADDR="$(get_ip_addr ${IPV4_FILE})"
+                    post_ip_addr ${IPV4_ADDR} 4
+                fi
+                if [[ 0 -eq `curl -s6 ${IP_CHECK} 2>&1 >${IPV6_FILE};echo $?` ]]; then
+                    IPV6_ADDR="$(get_ip_addr ${IPV6_FILE})"
+                    post_ip_addr ${IPV6_ADDR} 6
+                fi
+                break
             fi
-            if [[ 0 -eq `curl -s6 ${IP_CHECK} 2>&1 >${IPV6_FILE};echo $?` ]]; then
-                IPV6_ADDR="$(get_ip_addr ${IPV6_FILE})"
-                post_ip_addr ${IPV6_ADDR} 6
-            fi
-            break
+            sleep $SLEEP_S
+        done
+        if [[ 0 -eq `good_ipv6 ${IPV6_STAT}` ]]; then
+            echo $(basename $0)[$$] $(cat ${IPV6_STAT})
+        else
+            echo $(basename $0)[$$] ipv6 tunnel is failed
         fi
-        sleep $SLEEP_S
-    done
-    if [[ 0 -eq `good_ipv6 ${IPV6_STAT}` ]]; then
-        echo $(basename $0)[$$] $(cat ${IPV6_STAT})
     else
-        echo $(basename $0)[$$] ipv6 tunnel is failed
+        IPV4_ADDR=$(curl -s4 ${IPV4_ISP})
+        ssh ${SSH_LOGGER} "logger -p local3.info -t $(basename $0)[$$] ${IPV4_ADDR}"
+        echo $(basename $0)[$$] ${IPV4_ADDR}
     fi
+
 else
-    IPV4_ADDR=$(curl -s4 ${IPV4_ISP})
-    ssh ${SSH_LOGGER} "logger -p local3.info -t $(basename $0)[$$] ${IPV4_ADDR}"
-    echo $(basename $0)[$$] ${IPV4_ADDR}
+    curl -v4 ${IPV6_TUNNEL}
 fi
