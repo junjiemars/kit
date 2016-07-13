7fd7dc2b81026ab255b74605d0b707c4bf449d9d
南山竹 <junjiemars@gmail.com>
2016-07-13 16:29:11 +0800

    fix: failed to write body

--- win/setup-bash.sh
+++ win/setup-bash.sh
@@ -5,13 +5,22 @@ echo "begin setup `uname -s` shell env on Windows ..."
 GITHUB_H='https://raw.githubusercontent.com/junjiemars/kit/master'
 
 save_as() {
-  test -f ~/$1 && mv ~/$1 ~/$1.b0
+  test -f $HOME/$1 && mv ~/$1 $HOME/$1.b0
 }
 
-`save_as '.bash_aliases'` && curl -O ${GITHUB_H}/ul/.bash_aliases
-`save_as '.bash_paths'` && curl -O ${GITHUB_H}/win/.bash_paths
-`save_as '.bashrc'` && curl -O ${GITHUB_H}/win/.bashrc
-`save_as '.bash_profile'` && curl -O ${GITHUB_H}/win/.bash_profile
-`save_as '_vimrc'` && curl -O ${GITHUB_H}/win/_vimrc
+`save_as '.bash_aliases'` && \
+  curl ${GITHUB_H}/ul/.bash_aliases > $HOME/.bash_aliases
+
+`save_as '.bash_paths'` && \
+  curl ${GITHUB_H}/win/.bash_paths > $HOME/.bash_paths
+
+`save_as '.bashrc'` && \
+  curl -O ${GITHUB_H}/win/.bashrc > $HOME/.bashrc
+
+`save_as '.bash_profile'` && \
+  curl -O ${GITHUB_H}/win/.bash_profile > $HOME/.bash_profile
+
+`save_as '_vimrc'` && \
+  curl -O ${GITHUB_H}/win/_vimrc > $HOME/_vimrc
 
 echo "setup `uname -s` shell env on Windows successed"
