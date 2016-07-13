ec596d0d94095427c7f77657b60eee4ee2449604
南山竹 <junjiemars@gmail.com>
2016-07-13 17:19:31 +0800

    refine setup-bash for windows

--- win/setup-bash.sh
+++ win/setup-bash.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
+# usage: sh <(curl https://raw.githubusercontent.com/junjiemars/kit/master/win/setup-bash.sh)
 
-echo "begin setup `uname -s` shell env on Windows ..."
 
 GITHUB_H='https://raw.githubusercontent.com/junjiemars/kit/master'
+curl='curl -s '
+declare -a BASH_S=(\
+	'.bash_aliases' \
+	'.bash_paths' \
+	'.bashrc' \
+	'.bash_profile' \
+	'_vimrc')
 
 save_as() {
   test -f $HOME/$1 && mv ~/$1 $HOME/$1.b0
 }
 
-`save_as '.bash_aliases'` && \
-  curl ${GITHUB_H}/ul/.bash_aliases > $HOME/.bash_aliases
+BEGIN=`date +%s`
+echo "setup `uname -s` shell env on Windows ..."
 
-`save_as '.bash_paths'` && \
-  curl ${GITHUB_H}/win/.bash_paths > $HOME/.bash_paths
+for i in ${BASH_S[@]}; do
+	`save_as $i`
+done
 
-`save_as '.bashrc'` && \
-  curl ${GITHUB_H}/win/.bashrc > $HOME/.bashrc
+${curl} ${GITHUB_H}/ul/.bash_aliases > $HOME/.bash_aliases
+${curl} ${GITHUB_H}/win/.bash_paths > $HOME/.bash_paths
+${curl} ${GITHUB_H}/win/.bashrc > $HOME/.bashrc
+${curl} ${GITHUB_H}/win/.bash_profile > $HOME/.bash_profile
+${curl} ${GITHUB_H}/win/_vimrc > $HOME/_vimrc
 
-`save_as '.bash_profile'` && \
-  curl ${GITHUB_H}/win/.bash_profile > $HOME/.bash_profile
+END=`date +%s`
 
-`save_as '_vimrc'` && \
-  curl ${GITHUB_H}/win/_vimrc > $HOME/_vimrc
-
-echo "setup `uname -s` shell env on Windows successed"
+echo 
+echo "... elpased $(( ${END}-${BEGIN} )) seconds, successed."
