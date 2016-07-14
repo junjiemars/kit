98cce5cbd6128ca3aff0ba3ffd3ca487a483a96a
南山竹 <junjiemars@gmail.com>
2016-07-15 00:54:18 +0800

    refine save_as for setup-bash.sh

--- ul/setup-bash.sh
+++ ul/setup-bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # usage: bash <(curl https://raw.githubusercontent.com/junjiemars/kit/master/ul/setup-bash.sh)
-# if ur `sh' is not dash use `sh' indeed
+# if ur `sh' is not `dash' use `sh' instead.
 
 GITHUB_H='https://raw.githubusercontent.com/junjiemars/kit/master'
 PLATFORM=`uname -s 2>/dev/null`
@@ -8,7 +8,7 @@ curl='curl -s '
 declare -a BASH_S=(\
   '.bash_aliases' \
   '.bash_paths' \
-	'.bashrc' \
+  '.bashrc' \
   '.profile' \
   '.bash_profile' \
   '.bash_logout' \
@@ -17,14 +17,28 @@ declare -a BASH_S=(\
   )
 
 save_as() {
-  test -f $HOME/$1 && cp $HOME/$1 $HOME/$1.b0
+  local f="$HOME/$1"
+  if [ -f ${f} ]; then
+    local l=`find $HOME -type f -maxdepth 1 -mindepth 1 -name "$1.b?" \
+            |sort -r|head -n1`
+    if [ "_${l}" == "_" ]; then
+      local n=0
+    else
+      local c="${l:$(( ${#l}-1 )):1}"
+      local n=$(( ${c}+1 ))
+      if [[ ${n} -gt 2 ]]; then
+        let n=0
+      fi
+    fi
+    cp "${f}" "${f}.b${n}"
+  fi
 }
 
 BEGIN=`date +%s`
 echo "setup $PLATFORM bash env ..."
 
 for i in ${BASH_S[@]}; do
-	`save_as $i`
+  `save_as $i`
 done
 
 ${curl} ${GITHUB_H}/ul/.bash_aliases > $HOME/.bash_aliases
