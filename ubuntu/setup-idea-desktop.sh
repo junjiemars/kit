fb8cf017c2c16a013eafb7292910fba804d4eec1
南山竹 <junjiemars@gmail.com>
2016-07-25 13:34:37 +0800

    add idea setup for ubuntu desktop env

---
+++ ubuntu/setup-idea-desktop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# usage: sudo bash <(curl https://raw.githubusercontent.com/junjiemars/unbuntu/master/setup-idea-desktop.sh)
+
+IDEA_HOME=${IDEA_HOME:-/opt/open/idea/idea-IC-162.1121.32}
+
+# make idea run from terminal
+IDEA_TTY='/usr/local/bin/idea'
+cat <<END >${IDEA_TTY}
+#!/bin/bash
+${IDEA_HOME}/bin/idea.sh \$@ 2>&1 1>/tmp/idea.log
+END
+chmod u+x ${IDEA_TTY}
+
+# make idea run from desktop
+IDEA_DESKTOP='/usr/share/applications/idea.desktop'
+cat <<END >${IDEA_DESKTOP}
+[Desktop Entry]
+Name=IntelliJ IDEA
+Exec=${IDEA_HOME}/bin/idea.sh
+Type=Application
+StartupNotify=true
+Categories=Development;
+Comment=IntelliJ IDEA `date +%G`
+Path=/tmp
+Icon=${IDEA_HOME}/bin/idea.png
+Name[en_US]=IntelliJ IDEA
+END
+chmod u+x ${IDEA_DESKTOP}
