c071d7f82eebb6676659f18f7cfefe4921053f16
南山竹 <junjiemars@gmail.com>
2016-07-28 12:43:46 +0800

    refine install-java-kits script

--- ul/install-java-kits.sh
+++ ul/install-java-kits.sh
@@ -2,16 +2,33 @@
 
 PREFIX=${PREFIX:-'/opt/run/bin'}
 OPEN_DIR=${OPEN_DIR:-'/opt/open'}
-ANT_URL=${ANT_URL:-'https://github.com/apache/ant.git'}
-BOOT_URL=${BOOT_URL:-'https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh '}
+KITS=(
+      'install_ant'
+      #'install_maven'
+      #'install_boot'
+      #'install_gradle'
+      #'install_groovy'
+      #'install_scala'
+)
 
-# install ant
-ANT_HOME="${OPEN_DIR}/ant"
-cd "${OPEN_DIR}" && \
-git clone ${ANT_URL} && \
-cd "${ANT_HOME}" && bash ./bootstrap.sh && \
-echo -e "PATH=$PATH:$ANT_HOME" >> $HOME/.bash_paths
 
-## install boot
-#cd "${PREFIX}" && \
-#curl -fsSLo boot ${BOOT_URL} && chmod 755 boot
+install_ant() {
+  ANT_HOME="${OPEN_DIR}/ant"
+  ANT_URL=${ANT_URL:-'https://github.com/apache/ant.git'}
+
+  cd "${OPEN_DIR}" && \
+  git clone ${ANT_URL} && \
+  cd "${ANT_HOME}" && bash ./bootstrap.sh && \
+  echo -e "PATH=$PATH:$ANT_HOME" >> $HOME/.bash_paths
+}
+
+install_boot() {
+  BOOT_URL=${BOOT_URL:-'https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh '}
+
+  cd "${PREFIX}" && \
+  curl -fsSLo boot ${BOOT_URL} && chmod 755 boot
+}
+
+for i in "${KITS[@]}"; do
+  echo -e "install ${i} ...\n" && `${i}` && echo -e "\n${i} installed."
+done
