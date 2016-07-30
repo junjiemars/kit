ff21e43674099ab6b3c4cd17ede41c87d1310370
南山竹 <junjiemars@gmail.com>
2016-07-30 22:32:09 +0800

    remove set-java-home

--- ul/set-java-home.sh
+++
@@ -1,7 +0,0 @@
-os=$(uname -s)
-if [ ${os} = "darwin" ]; then
-    export JAVA_HOME=$(/usr/libexec/java_home)
-elif [ ${os} = "linux" ]; then
-    export JAVA_HOME=$(readlink -f `type -p java`|sed 's:/bin/java::g')
-fi
-
