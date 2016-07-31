d1737c6e23009d6776796af6429c32f81bbfa36a
南山竹 <junjiemars@gmail.com>
2016-07-31 16:03:25 +0800

    add install_scala

--- ul/install-java-kits.sh
+++ ul/install-java-kits.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-PREFIX=${PREFIX:-'/opt/run'}
-OPEN_DIR=${OPEN_DIR:-'/opt/open'}
+PREFIX=${PREFIX:-'/opt'}
+RUN_DIR=${RUN_DIR:-"${PREFIX}/run"}
+OPEN_DIR=${OPEN_DIR:-"${PREFIX}/open"}
 
 HAS_ANT=${HAS_ANT:-0}
 HAS_MAVEN=${HAS_MAVEN:-0}
 HAS_BOOT=${HAS_BOOT:-0}
 HAS_GRADLE=${HAS_GRADLE:-0}
 HAS_GROOVY=${HAS_GROOVY:-0}
-HAS_SCALA=${HAS_SCALA:-0}
+HAS_SCALA=${HAS_SCALA:-0}       # 2.11.8
 
 declare -a KITS=()
 
 append_paths() {
-  [ `echo $PATH | tr ':' '\n' | grep "^$1$" &>/dev/null; echo $?` ] && \
-    echo -e "PATH=$PATH:$1" >> $HOME/.bash_paths && \
-    . $HOME/.bashrc
+  local f_paths="$HOME/.bash_paths"
+  if [ 0 -ne `echo "$PATH" | tr ':' '\n' | grep "^$1$" &>/dev/null; echo $?` ]; then 
+    if [ 0 -eq `grep "^PATH='.*'" ${f_paths} &>/dev/null; echo $?` ]; then
+      sed -i 's#^PATH='.*'#PATH=$PATH:$1#g' "${f_paths}"
+    else
+      echo -e "PATH='$PATH:$1'" >> "${f_paths}"
+    fi
+    . "${f_paths}"
+  fi
 }
 
 append_vars() {
-  . $HOME/.bash_vars
-  [ `env | grep "^${1}$" &>/dev/null; echo $?` ] && \
-    echo -e "export $1" >> $HOME/.bash_vars && \
-    . $HOME/.bash_vars
+  local f_vars="$HOME/.bash_vars"
+  local var="export $1"
+  [ 0 -eq `grep "${var}" "${f_vars}" &>/dev/null; echo $?` ] || \
+    echo -e "${var}" >> "${f_vars}"
 }
 
 git_repo_exists() {
@@ -45,7 +52,7 @@ install_maven() {
   local maven_home="${OPEN_DIR}/maven"
   local maven_url='https://github.com/apache/maven.git'
   local maven_ver='maven-3.2.6'
-  local bin_dir="${PREFIX}/bin/m2"
+  local bin_dir="${RUN_DIR}/bin/m2"
   
   [ -d "${bin_dir}" ] || mkdir -p "${bin_dir}"
   rm -r "${bin_dir}/*"
@@ -65,7 +72,7 @@ install_maven() {
 
 install_boot() {
   local boot_url='https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh '
-  local bin_dir="${PREFIX}/bin"
+  local bin_dir="${RUN_DIR}/bin"
 
   [ -f "${bin_dir}/boot" ] || \
     curl -fsSLo ${bin_dir}/boot ${boot_url} && \
@@ -75,13 +82,12 @@ install_boot() {
 install_gradle() {
   local gradle_url='https://github.com/gradle/gradle.git'
   local gradle_home="${OPEN_DIR}/gradle"
-  local bin_ln="${PREFIX}/bin/gradlew"
+  local bin_ln="${RUN_DIR}/bin/gradlew"
 
   [ -f "${gradle_home}/gradlew" ] || \
     git clone --depth=1 ${gradle_url} ${gradle_home}
 
   [ -L "${bin_ln}" ] && rm "${bin_ln}"
-  . $HOME/.bashrc
   [ 0 -ne `type -p gradlew &>/dev/null; echo $?` ] && \
     ln -s ${gradle_home}/gradlew ${bin_ln}
 }
@@ -100,16 +106,20 @@ install_groovy() {
 }
 
 install_scala() {
-  local scala_url='https://github.com/scala/scala.git'
+  local scala_tgz="scala-${HAS_SCALA}.tgz"
+  local scala_url="http://downloads.lightbend.com/scala/${HAS_SCALA}/${scala_tgz}"
   local scala_home="${OPEN_DIR}/scala"
 
-  [ -f "${scala_home}/build.xml" ] || \
-    git clone --depth=1 ${scala_url} ${scala_home}
+  [ -d "${scala_home}" ] || mkdir -p "${scala_home}"
+  [ -f "${scala_home}/${scala_tgz}" ] || \
+    curl ${scala_url} -o "${scala_home}/${scala_tgz}" && \
+    tar xf "${scala_home}/${scala_tgz}" -C "${scala_home}" --strip-components=1
 
-  . $HOME/.bashrc
-  [ 0 -ne `type -p scala &>/dev/null; echo $?` ] && \
-    cd ${scala_home} && \
-    ant 
+  [ -d "${RUN_DIR}/share/man/man1" ] || mkdir -p "${RUN_DIR}/share/man/man1" 
+  cp -R "${scala_home}/man/man1/." "${RUN_DIR}/share/man/man1/"
+ 
+  append_vars "SCALA_HOME=${scala_home}"
+  append_paths "${scala_home}/bin"
 }
 
 [ 0 -lt "${HAS_ANT}" ]      && KITS+=('install_ant')
@@ -117,11 +127,10 @@ install_scala() {
 [ 0 -lt "${HAS_BOOT}" ]     && KITS+=('install_boot')
 [ 0 -lt "${HAS_GRADLE}" ]   && KITS+=('install_gradle')
 [ 0 -lt "${HAS_GROOVY}" ]   && KITS+=('install_groovy')
-[ 0 -lt "${HAS_SCALA}" ]    && KITS+=('install_scala')
-
+[ "0" != "${HAS_SCALA}" ]       && KITS+=('install_scala')
 
 for i in "${KITS[@]}"; do
   echo -e "# ${i} ..." 
-  ${i}  
+  [ -d "${OPEN_DIR}" ] || mkdir "${OPEN_DIR}" && ${i}  
   echo -e "# ${i} completed."
 done
