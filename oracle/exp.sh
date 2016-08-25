dec3daf484ac23a9b83c593f467a2a9a334ed397
南山竹 <junjiemars@gmail.com>
2016-08-25 16:35:03 +0800

    refine

--- oracle/exp.sh
+++ oracle/exp.sh
@@ -5,11 +5,12 @@
 # note: suit for multiple Oracle coexisting env
 #------------------------------------------------
 
-export ORACLE_HOME=${ORACLE_HOME:-"/opt/oracle/u01/app/oracle/product/11.2.0/xe"}
+export ORACLE_HOME=${ORACLE_HOME:-"/d/opt/oracle/product/11.2.0/server"}
 ORACLE_SID=${ORACLE_SID:-"XE"}
 
 export LD_LIBRARY_PATH=${ORACLE_HOME%/}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 export PATH=${ORACLE_HOME%/}/bin${PATH:+:$PATH}
+
 export NLS_LANG=${NLS_LANG:-'AMERICAN_AMERICA.UTF8'}
 #export NLS_CHARACTERSET=${NLS_CHARACTERSET:-"AL32UTF8"} # AL16UTF8
 #export NLS_LENGTH_SEMANTICS=${NLS_LENGTH_SEMANTICS:-"CHAR"} # BYTE
--- oracle/sql/move-tablespace.sql
+++ oracle/sql/move-tablespace.sql
@@ -1,23 +1,23 @@
 set serveroutput on
 
-define ts_old_name=users
-define ts_new_name=ts_xws
-define ts_old_iname=users
-define ts_new_iname=ts_xws
+define ts_src_name=users
+define ts_dst_name=ts_xws
+define ts_src_iname=users
+define ts_dst_iname=ts_xws
 
 declare
 
   cursor i_tables is 
     select table_name from user_tables 
-    where tablespace_name = '&&ts_old_name';
+    where tablespace_name = '&&ts_src_name';
 
   cursor i_indexes is 
     select index_name from user_indexes 
-    where tablespace_name = '&&ts_old_iname' and index_type != 'lob';
+    where tablespace_name = '&&ts_src_iname' and index_type != 'lob';
 
   cursor i_lob is 
     select table_name, column_name from user_lobs 
-    where tablespace_name = '&&ts_old_name';
+    where tablespace_name = '&&ts_src_name';
 
   cursor i_indexes_invalid is 
     select index_name from user_indexes 
@@ -26,11 +26,11 @@ declare
 begin
 
   dbms_output.put_line('------------------------');
-  dbms_output.put_line('! moving tables tablespace from &&ts_old_name to &&ts_new_name...');
+  dbms_output.put_line('! moving tables tablespace from &&ts_src_name to &&ts_dst_name...');
   for t in i_tables loop
     begin
-      execute immediate 'alter table ' || t.table_name || ' move tablespace &&ts_new_name';
-      dbms_output.put_line('# '||t.table_name||' -> tablespace: &&ts_new_name');
+      execute immediate 'alter table ' || t.table_name || ' move tablespace &&ts_dst_name';
+      dbms_output.put_line('# '||t.table_name||' -> tablespace: &&ts_dst_name');
     exception
       when others then
         dbms_output.put_line('# '||sqlcode||'@'||t.table_name||' -> '||sqlerrm);
@@ -38,11 +38,11 @@ begin
   end loop;
 
   dbms_output.put_line('------------------------');
-  dbms_output.put_line('! moving indexes tablespace from &&ts_old_iname to &&ts_new_iname...');
+  dbms_output.put_line('! moving indexes tablespace from &&ts_src_iname to &&ts_dst_iname...');
   for i in i_indexes loop
     begin
-      execute immediate 'alter index ' || i.index_name || ' rebuild tablespace &&ts_new_iname';
-      dbms_output.put_line('# '||i.index_name||' -> tablespace: &&ts_new_iname');
+      execute immediate 'alter index ' || i.index_name || ' rebuild tablespace &&ts_dst_iname';
+      dbms_output.put_line('# '||i.index_name||' -> tablespace: &&ts_dst_iname');
     exception
       when others then
         dbms_output.put_line('# '||sqlcode||'@'||i.index_name||' -> '||sqlerrm);
@@ -50,11 +50,11 @@ begin
   end loop;
 
   dbms_output.put_line('------------------------');
-  dbms_output.put_line('! moving lobs tablespace from &&ts_old_name to &&ts_new_name...');
+  dbms_output.put_line('! moving lobs tablespace from &&ts_src_name to &&ts_dst_name...');
   for l in i_lob loop
     begin
-      execute immediate 'alter table ' || l.table_name || ' move lob(' || l.column_name || ') store as (tablespace &&ts_new_name)';
-      dbms_output.put_line('# '||l.table_name||'.'||l.column_name||' -> tablespace: &&ts_new_name');
+      execute immediate 'alter table ' || l.table_name || ' move lob(' || l.column_name || ') store as (tablespace &&ts_dst_name)';
+      dbms_output.put_line('# '||l.table_name||'.'||l.column_name||' -> tablespace: &&ts_dst_name');
     exception
       when others then
         dbms_output.put_line('# '||sqlcode||'@'||l.table_name||'.'||l.column_name||' -> '||sqlerrm);
@@ -62,11 +62,11 @@ begin
   end loop;
 
   dbms_output.put_line('------------------------');
-  dbms_output.put_line('! rebuild indexes on tablespace &&ts_new_name...');
+  dbms_output.put_line('! rebuild indexes on tablespace &&ts_dst_name...');
   for b in i_indexes_invalid loop
     begin
       execute immediate 'alter index ' || b.index_name || ' rebuild';
-      dbms_output.put_line('# '||b.index_name||' ^ tablespace: &&ts_new_name');
+      dbms_output.put_line('# '||b.index_name||' ^ tablespace: &&ts_dst_name');
     exception
       when others then
         dbms_output.put_line('# '||sqlcode||'@'||b.index_name||' ^ '||sqlerrm);
