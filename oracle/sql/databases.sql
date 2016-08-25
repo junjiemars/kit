ca2af001b8f1d809eadae45707032b75bc9b9667
南山竹 <junjiemars@gmail.com>
2016-08-25 09:49:30 +0800

    format list databases

--- oracle/sql/databases.sql
+++ oracle/sql/databases.sql
@@ -3,8 +3,19 @@
 -- target: list databases 
 --------------------------------------------------
 
+column OPEN_MODE format a12
+column DATABASE_ROLE heading DATABASE|ROLE format a10
+column OPEN_MODE heading OPEN|MODE format a10 word_wrap
+column PLATFORM_NAME heading PLATFORM|NAME format a16 word_wrap
 
-select * from v$database
+select 
+    name
+  , created
+  , log_mode
+  , open_mode
+  , database_role
+  , platform_name
+from v$database
   order by name
 ;
 
