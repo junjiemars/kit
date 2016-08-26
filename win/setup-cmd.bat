113444756a4d09a5be23da3a42f3c64acbf3d488
南山竹 <junjiemars@gmail.com>
2016-08-26 10:23:11 +0800

    fix bug: reg add %%

--- win/setup-cmd.bat
+++ win/setup-cmd.bat
@@ -14,8 +14,8 @@ reg add "HKCU\Console" /v FaceName /t REG_SZ /d "White Rabbit" /f
 reg add "HKCU\Console" /v FontSize /t REG_DWORD /d 14 /f
 reg add "HKCU\Console" /v QuickEdit /t REG_DWORD /d 1 /f
 
-reg add "HKCU\Console/%%SystemRoot/%%_system32_cmd.exe" /v CodePage /t REG_DWORD /d 65001 /f
+reg add "HKCU\Console\%%SystemRoot%%_system32_cmd.exe" /v CodePage /t REG_DWORD /d 65001 /f
 reg add "HKCU\Console\%%SystemRoot%%_system32_cmd.exe" /v FaceName /t REG_SZ /d "White Rabbit" /f
-reg add "HKCU\Console/%%SystemRoot/%%_system32_cmd.exe" /v FontSize /t REG_DWORD /d 14 /f
+reg add "HKCU\Console\%%SystemRoot%%_system32_cmd.exe" /v FontSize /t REG_DWORD /d 14 /f
 
 
