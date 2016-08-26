f83d73ca22991180d3131d57dd32c1efce7e2187
南山竹 <junjiemars@gmail.com>
2016-08-26 10:03:28 +0000

    refine sql scripts

--- oracle/sql/check-column-privileges.sql
+++ oracle/sql/check-column-privileges.sql
@@ -1,5 +1,13 @@
--- check
--- grant select on user_col_privs/dba_col_privs to &&user_name
+--------------------------------------------------
+-- author: junjiemars@gmail.com
+-- target: grant select on 
+--         user_col_privs/dba_col_privs 
+--         to &&user_name
+--------------------------------------------------
 
-select * from user_column_privs
-  where table_name='&&table_name'
+--define table_name=
+
+select * 
+from user_col_privs
+  where table_name=upper('&&table_name')
+;
--- oracle/sql/unlock-user.sql
+++ oracle/sql/unlock-user.sql
@@ -1,8 +1,12 @@
--- unlock user
 
-define user_name=hr
-define user_password=hr
+--------------------------------------------------
+-- author: junjiemars@gmail.com
+-- target: unlock user
+--------------------------------------------------
 
-ALTER USER &&user_name IDENTIFIED BY &&user_password;
-ALTER USER &&user_name ACCOUNT UNLOCK;
+--define user_name=hr
+--define user_password=hr
+
+alter user &&user_name identified by &&user_password;
+alter user &&user_name account unlock;
 
