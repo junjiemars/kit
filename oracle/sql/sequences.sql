0927e48b1c6a2e2c118fcc47194849a2cd5a7d17
南山竹 <junjiemars@gmail.com>
2016-09-09 15:53:09 +0000

    format for sequences

--- oracle/sql/sequences.sql
+++ oracle/sql/sequences.sql
@@ -5,11 +5,11 @@
 --------------------------------------------------
 
 column sequence_name format a30
-column min_value heading MIN|VALUE format 999
+column min_value heading MIN|VALUE format 9999
 column max_value heading MAX|VALUE
 column increment_by heading INCR|BY
-column cache_size heading CACHE|SIZE format 999
-column last_number heading LAST|NUMBER format 99
+column cache_size heading CACHE|SIZE format 9999
+column last_number heading LAST|NUMBER format 9999
 
 select *
 	from user_sequences
