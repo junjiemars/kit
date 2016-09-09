fafc7e7613964c198adb6749069cd2469a68e201
南山竹 <junjiemars@gmail.com>
2016-09-09 17:17:41 +0800

    drop-user failed: ora-00942

--- oracle/sql/drop-user.sql
+++ oracle/sql/drop-user.sql
@@ -6,8 +6,13 @@
 
 --define user_name=who
 
+--Note:
+--If failed with ORA-00942: Table or view doesn´t exist 
+--then connect as sysdba, and run 
+--@$ORACLE_HOME/rdbms/admin/catqueue.sql
+--and do it again, good luck!
 
-drop user &&user_name cascade
-;
+drop user &&user_name cascade;
+
 
 
