ae0bf2210b5ceb0c0803a30a45859c46834d87ff
南山竹 <junjiemars@gmail.com>
2016-09-09 15:22:24 +0000

    add start option variable

--- oracle/sql/create-sequence.sql
+++ oracle/sql/create-sequence.sql
@@ -9,12 +9,13 @@
 
 
 --define seq_name=which
+--define seq_start=1
 --define seq_incr=1
 
 create sequence &&seq_name 
 	minvalue 1
 	maxvalue 99999999999999999
-	start with 1
+	start with &&seq_start
 	increment by &&seq_incr
 	cache 20
 ;
