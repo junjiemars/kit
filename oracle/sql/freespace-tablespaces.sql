fc916f8e5096952e620f047440b9e6177176b5ec
南山竹 <junjiemars@gmail.com>
2016-09-13 03:30:18 +0000

    refine space/freespace for tablespace

---
+++ oracle/sql/freespace-tablespaces.sql
@@ -0,0 +1,35 @@
+
+--------------------------------------------------
+-- author: junjiemars@gmail.com
+-- target: free space % by dba 
+--------------------------------------------------
+
+
+--define ts_name=which
+
+column tablespace_name format a20
+
+select df.tablespace_name "TABLESPACE_NAME",
+       df.bytes / (1024 * 1024) "SIZE (MB)",
+       sum(fs.bytes) / (1024 * 1024) "FREE (MB)",
+       nvl(round(sum(fs.bytes) * 100 / df.bytes),1) "% FREE",
+       round((df.bytes - sum(fs.bytes)) * 100 / df.bytes) "% USED"
+  from dba_free_space fs,
+       (select tablespace_name, sum(bytes) bytes
+          from dba_data_files
+         group by tablespace_name) df
+ where fs.tablespace_name (+)  = df.tablespace_name
+ group by df.tablespace_name, df.bytes
+union all
+select df.tablespace_name tspace,
+       fs.bytes / (1024 * 1024),
+       sum(df.bytes_free) / (1024 * 1024),
+       nvl(round((sum(fs.bytes) - df.bytes_used) * 100 / fs.bytes), 1),
+       round((sum(fs.bytes) - df.bytes_free) * 100 / fs.bytes)
+  from dba_temp_files fs,
+       (select tablespace_name, bytes_free, bytes_used
+          from v$temp_space_header
+         group by tablespace_name, bytes_free, bytes_used) df
+ where fs.tablespace_name (+)  = df.tablespace_name
+ group by df.tablespace_name, fs.bytes, df.bytes_free, df.bytes_used
+ order by 4 desc;
--- oracle/sql/space-tablespace.sql
+++ oracle/sql/space-tablespace.sql
@@ -9,14 +9,16 @@
 
 column tablespace_name format a20
 column relative_fno heading RELATIVE|FNO
+break on tablespace_name
 
 select 
-		tablespace_name
-	,	file_id
+	  tablespace_name
+	, file_id
 	,	block_id
 	, bytes
 	, blocks
 	,	relative_fno
 	from user_free_space
 	where tablespace_name=upper('&&ts_name')
+	order by block_id
 ;
