0c3772f977593d2ab39764551d2e7c08b60d476a
南山竹 <junjiemars@gmail.com>
2016-09-19 15:15:26 +0800

    add checking hostednetwork

--- win/ipv4-static.bat
+++ win/ipv4-static.bat
@@ -1,18 +1,39 @@
+@echo off
+
 set ip=<ipv4-addr-1>
 REM set ip=<ipv4-addr-2>
+set if_name="Local Area Connection"
+set mask="<netmask>"
+set gw="<gateway>"
 set dns0=8.8.4.4
 set dns1=74.82.42.42
+set ssid="<ssid>"
+set key="<passwd>"
 
-netsh interface ipv4 set address name="Local Area Connection" static %ip% 255.255.252.0 192.168.20.254 1
+echo "set ipv4 address ..."
+netsh interface ipv4 set address name=%if_name% static %ip% %mask% %gw% 1
 
-netsh interface ipv4 set dnsserver name="Local Area connection" static %dns0% primary
+echo "set dns server ..."
+netsh interface ipv4 set dnsserver %if_name% static %dns0% primary
+netsh interface ipv4 add dnsserver %if_name% %dns1%
 
-netsh interface ipv4 add dnsserver name="Local Area connection" %dns1%
+netsh wlan show driver | findstr "Hosted network" | findstr "No"
+if errorlevel 0 (
+   echo "checking hosted network ...no"
+) else (
+   echo "set hosted network ..."
+   netsh wlan show hostednetwork | findstr %hot_name%
+   if errorlevel 1 (
+      netsh wlan set hostednetwork mode=allow^
+      ssid=%ssid%^
+      key=%key% keyusage=pesistent
+   )
+   echo "start hosted network ..."
+   netsh wlan start hostednetwork
+)
 
-REM ping 1.1.1.1 -n 1 -w 5000 > nul REM sleep 5s
-REM netsh interface ipv4 delete route 10.0.0.0/8 "Local Area connection" 10.32.65.1 
-REM netsh interface ipv4 add route 10.0.0.0/8 "Local Area connection" 10.32.65.1 store=active
-REM netsh wlan start hostednetwork
 
+echo "flush dns cache ..."
 ipconfig /flushdns
 
+@echo on
