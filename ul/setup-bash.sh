2132553f168696994f6a554bdbf65e2f47d78740
南山竹 <junjiemars@gmail.com>
2016-11-18 13:35:28 +0000

    set vim path vars for msvc include

--- ul/setup-bash.sh
+++ ul/setup-bash.sh
@@ -60,26 +60,27 @@ set_vim_path_var() {
 
   echo -e "\n\" cc include path" >> $f
   for i in "${inc_lns[@]}"; do
-    echo -e "set path+=${i}" >> $f
+    echo "set path+=${i}" >> $f
   done
 }
 
 check_linux_cc_include() {
+	local vimrc=$1
   if `type -p cc &>/dev/null`; then
     local cc_out="`echo '' | cc -v -E 2>&1 >/dev/null - \
-            | awk '/#include <...> search starts here:/,/End of search list./'`"
-    if [ -n "$cc_out" ]; then
-      local inc_lns=()
-      IFS=$'\n'
-      for l in `echo -e "$cc_out"`; do
-        inc_lns+=($(echo -e "$l" | sed 's/^ //'))
-      done
-      unset IFS
-      local inc_ln="${#inc_lns[@]}"
-      if [[ 2 -lt "$inc_ln" ]]; then
-        local inc_paths=("${inc_lns[@]:1:$(( inc_ln-2  ))}")
-        set_vim_path_var "$1" "${inc_paths[@]}"
-      fi
+									| awk '/#include <...> search starts here:/,/End of search list./'`"
+    [ -n "$cc_out" ] || return 1
+
+		local inc_lns=()
+		IFS=$'\n'
+		for l in `echo "$cc_out"`; do
+			inc_lns+=($(echo "$l" | sed 's/^ //'))
+		done
+		unset IFS
+		local inc_ln="${#inc_lns[@]}"
+		if [[ 2 -lt "$inc_ln" ]]; then
+			local inc_paths=("${inc_lns[@]:1:$(( inc_ln-2  ))}")
+			set_vim_path_var "${vimrc}" "${inc_paths[@]}"
     fi
   fi
 }
@@ -88,41 +89,34 @@ to_posix_path() {
 	echo "\\$1" | \
 	  sed \
       -e 's#^\\\([a-zA-Z]\):\\#\\\l\1\\#' \
-      -e 's#\\#\/#g'  \
-      -e 's# #\\\\ #g'  \
-      -e 's#(#\\\\(#g'  \
-      -e 's#)#\\\\)#g'
+      -e 's#\\#\/#g'      \
+      -e 's# #\\\\\\ #g'  \
+      -e 's#(#\\\\\\(#g'  \
+      -e 's#)#\\\\\\)#g'
 }
 
 check_win_cc_include() {
   local vimrc="$1"
   local inc_bat="$2"
 
-  [ -f "$vimrc" ] || return 1
   [ -f "$inc_bat" ] || return 1
 
   local include=$($inc_bat "`env|grep 'VS[0-9][0-9]*COMNTOOLS'|sed 's#^VS[0-9]*COMNTOOLS=\(.*\)$#\1#g'`")
   [ -n "$include" ] || return 1
 
   include=$(echo $include | sed 's#\"##g')
-  #echo $include
   local inc_lns=()
   IFS=$';'
-  for l in `echo -e "$include"`; do
-    inc_lns+=(`to_posix_path $(echo -e "${l}" | sed 's/^ //')`)
+  for l in `echo "$include"`; do
+    inc_lns+=(`to_posix_path $(echo "${l}" | sed 's/^ //')`)
   done
   unset IFS
 
-  echo "${inc_lns[@]}"
   set_vim_path_var "${vimrc}" "${inc_lns[@]}"
-  ##for l in "${inc_lns[@]}"; do
-  ##  echo "$l"
-  ##done
-
-  
 }
 
 #check_win_cc_include "$HOME/.vimrc" "./win/vs-inc.bat"
+#check_linux_cc_include "$HOME/.vimrc"
 #to_posix_path "C:\Program Files (x86)\Windows Kits\10\include\10.0.10586.0\winrt"
 
 BEGIN=`date +%s`
@@ -142,7 +136,7 @@ case ${PLATFORM} in
     ${curl} ${GITHUB_H}/win/.bashrc -o $HOME/.bashrc
     ${curl} ${GITHUB_H}/ul/.vimrc -o $HOME/_vimrc && \
       cp $HOME/_vimrc $HOME/.vimrc
-    ${curl} ${GITHUB_H}/win/vs-inc.bat -o $HOME/vs-inc.bat
+    ${curl} ${GITHUB_H}/win/vs-inc.bat -o $HOME/.vs-inc.bat
     ;;
   *)
 		${curl} ${GITHUB_H}/ul/.bashrc -o $HOME/.bash_init
@@ -166,7 +160,7 @@ case ${PLATFORM} in
     check_linux_cc_include $HOME/.vimrc
     ;;
   MSYS_NT*)
-    #check_win_cc_include $HOME/.vimrc
+    check_win_cc_include $HOME/.vimrc $HOME/.vs-inc.bat
     ;;
   *)
     ;;
