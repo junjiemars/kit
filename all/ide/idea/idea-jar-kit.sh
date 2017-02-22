48ad3becf4e00cfa6129b640df1bde7ffe9731a4
南山竹 <junjiemars@gmail.com>
2017-02-22 18:03:37 +0800

    refined idea-jar-kit.sh, support independent deployment

--- all/ide/idea/idea-jar-kit.sh
+++ all/ide/idea/idea-jar-kit.sh
@@ -5,28 +5,61 @@
 #------------------------------------------------
 
 PREFIX=${PREFIX:-"`pwd`"}
-PLATFORM=`uname -s 2>/dev/null`
-R_DIR="`dirname $0`"
-S_DIR="${R_DIR%/}/linux"
-S_JAR=${S_JAR:-"settings.jar"}
 
-case "$PLATFORM" in
-	MSYS_NT*)
-		S_DIR="${R_DIR%/}/win"
-		;;
-	Darwin)
-		S_DIR="${R_DIR%/}/darwin"
-		;;
-esac
+function set_os_prefix() {
+	local p="$1"	
+	local os=`uname -s 2>/dev/null`
+
+	[ -z "$p" ] && p="$PREFIX"
+
+	case "$os" in
+		MSYS_NT*)
+			p="${p%/}/win"
+			;;
+		Darwin)
+			p="${p%/}/darwin"
+			;;
+		Linux)
+			p="${p%/}/linux"
+			;;
+	esac
+
+	echo "$p"
+}
+
+function usage() {
+  echo -e "Usage: $(basename $0) COMMAND [arg...]"
+  echo -e "       $(basename $0) [ -h | --help | -v | --version ]\n"
+  echo -e "Options:"
+  echo -e "  -h, --help\t\tPrint usage"
+  echo -e "A Idea settings extract/archive kit.\n"
+  echo -e "Commands:"
+  echo -e "\t-x|--extract\t\teXtract <settings.jar> to <PREFIX|somewhere>"
+  echo -e "\t-a|--archive\t\tarchive <settings> from git repo to <PREFIX|somewhere>"
+}
 
 case "$@" in
 	-x*|--extract=*)
-		echo "xyz|$2|abc"
-		[ -d "${PREFIX}" ] || mkdir -p "${PREFIX}"
-		cd "${PREFIX}" ; jar xvf "$2"	
+		if [ $# -lt 2 ]; then
+			usage	
+		else
+			p=$(set_os_prefix "$3")
+			[ -d "$p" ] || mkdir -p "$p"; 
+			echo "extracting [$2] to \n\t$p ..."
+			cd "$p" ; jar xvf "$2"
+		fi
 		;;
-	*)
-		jar cvfM "${PREFIX%/}/${S_JAR}" -C "${S_DIR}" .
+	-a*|--archive=*)
+		if [ $# -lt 2 ]; then
+			usage
+		else
+			p=$(set_os_prefix "$3")
+			echo "archiving [$2] to \n\t[$p] ..."
+			jar cvfM "${2%/}" -C "$p" .
+		fi
+		;;
+	-h|--help|*)
+		usage
 		;;
 esac
 
