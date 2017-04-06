a40eb3afbafb49ca1b0cc299c8397b5c3461a8ef
南山竹 <junjiemars@gmail.com>
2017-04-06 18:21:44 +0800

    defense coding

--- all/net/win/static-en-wlan-ipv4-js.ps1
+++ all/net/win/static-en-wlan-ipv4-js.ps1
@@ -16,58 +16,64 @@ $dns1 = '74.82.42.42'
 $wif = Get-NetAdapter -Name:"Wi-Fi"
 $wif_ip = '192.168.0.100'
 $wif_gw = '192.168.0.1'
-$wif_ip_prefix = '10.33.208.0/24'
-$wif_ip_prefix_mask = '255.255.255.0'
+$wif_ip_prefixes = ('10.32.0.0/16', '10.33.0.0/16')
 
 
 # Enable dhcp first
 $eif | Set-NetIPInterface -Dhcp:Enabled
 $wif | Set-NetIPInterface -Dhcp:Enabled
 
+# Remove enternet default ip address
+$ip = $eif | Get-NetIPAddress -AddressFamily:IPv4 
+if ($ip) {
+  $eif | Remove-NetIPAddress -IPAddress:$ip.IPAddress `
+                             -AddressFamily:IPv4 `
+                             -Confirm:$false
+}
+
 # Remove enternet default route
 $eif_dgw = $eif | Get-NetRoute -DestinationPrefix:'0.0.0.0/0' `
                                -ErrorAction:Continue 
-# -and $eif_dgw.NextHop -eq $eif_gw
 if ($eif_dgw) {
-    Remove-NetRoute -DestinationPrefix:'0.0.0.0/0' `
-                    -InterfaceIndex:$eif.ifIndex `
-                    -NextHop:$eif_dgw.NextHop `
-                    -Confirm:$false
-}
+  Remove-NetRoute -DestinationPrefix:'0.0.0.0/0' `
+                  -InterfaceIndex:$eif.ifIndex `
+                  -NextHop:$eif_dgw.NextHop `
+                  -Confirm:$false
+} 
 
-# Remove wifi default route
-$wif_dgw = $wif | Get-NetRoute -DestinationPrefix:'0.0.0.0/0' `
-                               -ErrorAction:Continue
-# -and $wif_dgw.NextHop -eq $wif_gw
-if ($wif_dgw) {
-    Remove-NetRoute -DestinationPrefix:'0.0.0.0/0' `
-                    -InterfaceIndex:$wif.ifIndex `
-                    -NextHop:$wif_dgw.NextHop `
-                    -Confirm:$false
-}
-
-# Remove wifi specified route
-$wif_dgw = $wif | Get-NetRoute -DestinationPrefix:$wif_ip_prefix `
-                               -ErrorAction:Continue
-
-if ($wif_dgw -and $wif_dgw.NextHop -eq $wif_ip_prefix) {
-    Remove-NetRoute -DestinationPrefix:$wif_ip_prefix `
-                    -InterfaceIndex:$wif.ifIndex `
-                    -NextHop:$wif_ip_prefix `
-                    -Confirm:$false
-}
-
-
+# New enternet ip address
 $eif | New-NetIPAddress -IPAddress:$eif_ip `
                         -PrefixLength:$eif_ip_prefix_len `
-                        -DefaultGateway:$eif_gw 
+                        -DefaultGateway:$eif_gw `
+                        -Confirm:$false
 
+# New enternet dns
 $eif | Set-DNSClientServerAddress -ServerAddresses:($dns0,$dns1) `
                                   -ErrorAction:Continue `
                                   -Confirm:$false
 
-$wif | Set-NetRoute -DestinationPrefix:$wif_ip_prefix `
-                    -NextHop:$wif_gw `
-                    -Confirm:$false
+# Remove wifi default route
+$wif_dgw = $wif | Get-NetRoute -DestinationPrefix:'0.0.0.0/0' `
+                               -ErrorAction:Continue
+if ($wif_dgw) {
+  Remove-NetRoute -DestinationPrefix:'0.0.0.0/0' `
+                  -InterfaceIndex:$wif.ifIndex `
+                  -NextHop:$wif_dgw.NextHop `
+                  -Confirm:$false
+}
 
-#Get-NetRoute
+# Remove wifi specified route
+foreach ($ip_prefix in $wif_ip_prefixes) {
+  $wif_dgw = $null
+  $wif_dgw = $wif | Get-NetRoute -DestinationPrefix:$ip_prefix `
+                                 -ErrorAction:Continue
+  if ($wif_dgw -and $wif_dgw.NextHop -eq $ip_prefix) {
+    Remove-NetRoute -DestinationPrefix:$ip_prefix `
+                    -InterfaceIndex:$wif.ifIndex `
+                    -NextHop:$ip_prefix `
+                    -Confirm:$false
+  }
+  $wif | New-NetRoute -DestinationPrefix:$ip_prefix `
+                      -NextHop:$wif_gw `
+                      -Confirm:$false
+}
