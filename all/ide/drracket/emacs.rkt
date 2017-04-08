6f1913f53ce40ce365fb8e85ac5b177d1b72fdd9
南山竹 <junjiemars@gmail.com>
2017-04-08 13:01:36 +0800

    project settings: 'C-x s' on Darwin

--- all/ide/drracket/emacs.rkt
+++ all/ide/drracket/emacs.rkt
@@ -6,8 +6,53 @@ Emacs:
 * Edit>Keybindings>Add User-defined Keybindings...
 |#
 
-(keybinding "c:a:i" (λ (editor evt) (send editor auto-complete) ))
 
+#|
+(define (menu-bind key menu-item)
+  (keybinding
+   key
+   (λ (ed evt)
+     (define canvas (send ed get-canvas))
+     (when canvas
+       (define menu-bar (find-menu-bar canvas))
+       (when menu-bar
+         (define item (find-item menu-bar menu-item))
+         (when item
+           (define menu-evt
+             (new control-event%
+                  [event-type 'menu]
+                  [time-stamp
+                   (send evt get-time-stamp)]))
+           (send item command menu-evt)))))))
+ 
+(define/contract (find-menu-bar c)
+  (-> (is-a?/c area<%>) (or/c #f (is-a?/c menu-bar%)))
+  (let loop ([c c])
+    (cond
+      [(is-a? c frame%) (send c get-menu-bar)]
+      [(is-a? c area<%>) (loop (send c get-parent))]
+      [else #f])))
+ 
+(define/contract (find-item menu-bar label)
+  (-> (is-a?/c menu-bar%)
+      string?
+      (or/c (is-a?/c selectable-menu-item<%>) #f))
+  (let loop ([o menu-bar])
+    (cond
+      [(is-a? o selectable-menu-item<%>)
+       (and (equal? (send o get-plain-label) label)
+            o)]
+      [(is-a? o menu-item-container<%>)
+       (for/or ([i (in-list (send o get-items))])
+         (loop i))]
+      [else #f])))
+ 
+;(menu-bind "c:x:k" "Close Tab")
+;(menu-bind "c:a:i" "Complete Word")
+|#
+
+;(keybinding "c:x;k" (λ (editor evt) (send editor close-current-tab) ))
+(keybinding "c:a:i" (λ (editor evt) (send editor auto-complete) ))
 
 
 
