359cd742f3bf75a256ecf2ce41cedf466fa8de90
南山竹 <junjiemars@gmail.com>
2017-05-26 18:12:11 +0800

    works perfect
    1. start, auto install, --debug
    2. stop
    3. local, ssh, docker

--- ul/td.sh
+++ ul/td.sh
@@ -23,6 +23,7 @@ TC_SH="tc.sh"
 STOP_TIMEOUT="${STOP_TIMEOUT:3}"
 START_PORT="${START_PORT:-8080}"
 STOP_PORT="${STOP_PORT:-8005}"
+JPDA_PORT="${JPDA_PORT:-8000}"
 
 TO_WHERE=("local" "ssh" "docker")
 TW_IDX=
@@ -43,45 +44,10 @@ BUILD_OPTS="${BUILD_OPTS:-build}"
 SSH_USER="$USER"
 SSH_HOST=
 
-#TC_SH_ENV=${TC_SH_ENV:-"PREFIX=$PREFIX VER=$VER START_PORT=$START_PORT STOP_PORT=$STOP_PORT JPDA_PORT=$JPDA_PORT JAVA_OPTS=$JAVA_OPTS"}
+DOCKER_USER="$USER"
+DOCKER_HOST=
 
-
-
-#TO_BUILD=("no" "yes")
-
-
-#HAS_SSH=${HAS_SSH:-1}
-#SSH_USER=${SSH_USER:-}
-#SSH_HOST=${SSH_HOST:-}
-#
-## docker
-#HAS_DOCKER=${HAS_DOCKER:-1}
-#DOCKER_CONTAINER=${DOCKER_CONTAINER:-"java-dev"}
-#DOCKER_USER=${DOCKER_USER:-"u"}
-#DOCKER_TC_SH=${DOCKER_TC_SH:-"${OPT_RUN}/bin/$TC_SH"}
-#
-## war 
-#BUILD=${BUILD:-0}
-#BUILD_CMD=("gradlew")
-#BUILD_DIR="${BUILD_DIR:-.}"
-#BUILD_TASK="${BUILD_TASK:-build}"
-#
-#BUILD_LOG=${BUILD_LOG:-"td-build.log"}
-#WAR_TARGET=${WAR_TARGET:-"-Dwar.target=local"}
-#WAR_TASK="${WAR_TASK}"
-#WAR_DIR="${PREFIX}/${VER}/webapps/"
-#WAR_NAME=${WAR_NAME:-}
-#R_WAR_FILE="${WAR_DIR}${WAR_NAME}.war"
-#L_WAR_FILE="${BUILD_DIR%/}/${WAR_NAME}.war"
-#
-## platform
-#TD_CTRL_BAT="td-ctrl.bat"
-#TD_CTRL_SH="${TD_CTRL_BAT%.*}.sh"
-#TD_SHA1SUM_BAT="td-sha1sum.bat"
-#TD_SHA1SUM_SH="${TD_SHA1SUM_BAT%.*}.sh"
-#TD_RM_BAT="td-rm.bat"
-#R_TD_CTRL_SH="$OPT_RUN/bin/$TD_CTRL_SH"
-#R_TD_SHA1SUM_SH="$OPT_RUN/bin/$TD_SHA1SUM_SH"
+TD_SHA1SUM_SH="td_sha1sum.sh"
 
 
 usage() {
@@ -92,21 +58,30 @@ usage() {
   echo -e "  --version\t\t\t\tPrint version information and quit"
   echo -e "  --prefix\t\t\t\tcatalina prefix dir"
   echo -e "  --tomcat-version\t\t\ttomcat version, default is $VER"
-  echo -e "  --java-options\t\t\tjava options, JAVA_OPTS"
-  echo -e "  --tc-path\t\t\t\t\ttc.sh path"
   echo -e "  --tc-options\t\t\t\ttc.sh options"
+  echo -e "  --java-options\t\t\tjava options, JAVA_OPTS"
+  echo -e "  --debug\t\t\t\tstart tomcat in debug mode"
+  echo -e "  --local-war-path\t\t\tthe local path of the war"
+  echo -e "  --where\t\t\t\twhere to deploy, ${TO_WHERE[@]}"
+  echo -e "  --build\t\t\t\tforce to build, default is $BUILD"
+  echo -e "  --build-dir\t\t\t\tbuilding in where"
+  echo -e "  --build-cmd\t\t\t\twhich building tool to use, default is ${BUILD_CMD}"
+  echo -e "  --build-options\t\t\tbuilding options, default is ${BUILD_OPTS}"
+  echo -e "  --ssh-user\t\t\t\tssh login user, default is ${SSH_USER}"
+  echo -e "  --ssh-host\t\t\t\twhich ssh host to login"
+  echo -e "  --docker-user\t\t\t\tdocker container user, default is ${DOCKER_USER}"
+  echo -e "  --docker-host\t\t\t\tdocker container name"
   echo -e "  --start-port\t\t\t\ttomcat start port, default is $START_PORT"
   echo -e "  --stop-port\t\t\t\ttomcat stop port, default is $STOP_PORT"
-  echo -e "  --jpda-port\t\t\t\ttomcat debug port, default is $JPDA_PORT"
-  echo -e "  --war-path\t\t\t\twhere war file exists"
-  echo -e "  --where\t\t\t\twhere to deploy, ${TO_WHERE[@]}\n"
+  echo -e "  --jpda-port\t\t\t\ttomcat debug port, default is $JPDA_PORT\n"
   echo -e "A tiny-handy console for tomcat.\n"
   echo -e "Commands:"
   echo -e "  build\t\t\t\t\tbuild war which will be deployed"
   echo -e "  start\t\t\t\t\tstart a tomcat instance"
   echo -e "  stop\t\t\t\t\tstop a tomcat instance"
-  echo -e "  debug\t\t\t\t\tstart a tomcat instance in debug mode"
-  echo -e "  check\t\t\t\t\tcheck the environment of the tomcat instance"
+  echo -e "  check-env\t\t\t\tcheck the environment of the tomcat instance"
+  echo -e "  check-pid\t\t\t\tcheck the pid of the tomcat instance"
+  echo -e "  check-exist\t\t\t\tcheck the pid of the tomcat instance"
 }
 
 
@@ -118,61 +93,14 @@ export_java_opts() {
   export JAVA_OPTS=`echo "${JAVA_OPTS}" | tr -s " "`
 }
 
-
-gen_platform_sh() {
-	if [ ! -f $TD_SHA1SUM_SH ]; then
-		cat << END > $TD_SHA1SUM_SH
+function gen_docker_sha1sum_sh() {
+  if [ ! -f "$TD_SHA1SUM_SH" ]; then
+    cat << END > "$TD_SHA1SUM_SH"
 #!/bin/bash
-test -f $R_WAR_FILE && sha1sum $R_WAR_FILE | cut -d' ' -f1
+test -f \$1 && sha1sum \$1 | cut -d' ' -f1
 END
-		chmod u+x $TD_SHA1SUM_SH
-	fi
-
-
-	if [ ! -f $TD_CTRL_SH ]; then
-		cat << END > $TD_CTRL_SH
-#!/bin/bash
-export PREFIX=$PREFIX 
-export VER=$VER
-export START_PORT="$START_PORT"
-export STOP_PORT="$STOP_PORT"
-export JPDA_PORT="$JPDA_PORT"
-export JAVA_OPTS="$JAVA_OPTS"
-
-$DOCKER_TC_SH \$@
-END
-		chmod u+x $TD_CTRL_SH
-	fi
-
-	case $PLATFORM in
-		MSYS_NT*)
-			if [ ! -f $TD_CTRL_BAT ]; then
-				cat << END > $TD_CTRL_BAT
-@echo off
-docker exec $DOCKER_CONTAINER $R_TD_CTRL_SH %1 
-END
-				chmod u+x $TD_CTRL_BAT
-			fi
-
-			if [ ! -f $TD_SHA1SUM_BAT ]; then
-				cat << END > $TD_SHA1SUM_BAT
-@echo off
-docker exec $DOCKER_CONTAINER $R_TD_SHA1SUM_SH 
-END
-				chmod u+x $TD_SHA1SUM_BAT
-			fi
-
-			if [ ! -f $TD_RM_BAT ]; then
-				cat << END > $TD_RM_BAT
-@echo off
-docker exec $DOCKER_CONTAINER rm -rf ${WAR_DIR}${WAR_NAME} $R_WAR_FILE
-END
-				chmod u+x $TD_RM_BAT
-			fi
-			;;
-		*)
-			;;
-	esac
+    chmod u+x "$TD_SHA1SUM_SH"
+  fi
 }
 
 function local_bin_path() {
@@ -187,13 +115,23 @@ function local_bin_path() {
   fi
 }
 
+function remote_root_path() {
+  local p="${PREFIX%/}"
+  echo "$p"
+}
+
+function remote_ver_path() {
+  local p="`remote_root_path`/${VER%/}"
+  echo "$p"
+}
+
 function remote_war_path() {
-  local p="${PREFIX%/}/$VER/webapps/`basename $L_WAR_PATH`"
+  local p="`remote_ver_path`/webapps/`basename $L_WAR_PATH`"
   echo "$p"
 }
 
 function remote_bin_path() {
-  local p="${PREFIX%/}/$VER/bin/$1"
+  local p="`remote_ver_path`/bin/$1"
   echo "$p"
 }
 
@@ -209,7 +147,7 @@ function is_file_exist() {
       t=`ssh $SSH_USER@$SSH_HOST test -f $p &>/dev/null; echo $?`
       ;;
     docker)
-      
+      t=`docker exec -u $DOCKER_USER $DOCKER_HOST test -f $p &>/dev/null; echo $?`
       ;;
     *)
       t=`test -f $p &>/dev/null; echo $?`
@@ -220,6 +158,44 @@ function is_file_exist() {
     echo -e "# $lr[$p] exist   =true"
   else
     echo -e "! $lr[$p] exist   =false"
+  fi
+  return $t
+}
+
+function check_exist() {
+  local w="$1"
+  local t=
+
+  echo -e "+ check Tomcat[$VER] existing ..."
+
+  if [ "${TO_WHERE[$TW_IDX_LOCAL]}" = "$w" ]; then
+    control_tomcat check-exist "$w"
+    return $?
+  fi
+
+  local rp="`remote_bin_path catalina.sh`"
+  is_file_exist "$rp" "$w"
+  t=$?
+  if [ 0 -ne $t ]; then
+    echo -e "! check Tomcat[$VER] existing: catalina.sh no found  =failed"
+    return $t
+  fi
+
+  case "$w" in
+    ssh)
+      ssh $SSH_USER@$SSH_HOST $rp version
+      t=$?
+      ;;
+    docker)
+      docker exec -u $DOCKER_USER $DOCKER_HOST $rp version
+      t=$?
+      ;;
+  esac
+
+  if [ 0 -eq $t ]; then
+    echo -e "# check Tomcat[$VER] existing  =succeed"
+  else
+    echo -e "! check Tomcat[$VER] existing  =failed"
   fi
   return $t
 }
@@ -250,7 +226,22 @@ function control_tomcat() {
       t=$?
       ;;
     docker)
-      echo "docker"
+      tc="`remote_bin_path $TC_SH`"
+      is_samed_file "`local_bin_path $TC_SH`" "$tc" "$w"
+      if [ 0 -ne $? ]; then
+        transport_file "`local_bin_path $TC_SH`" "$tc" "$w"
+        t=$?
+        [ 0 -eq $t ] || return $t
+      fi
+
+      docker exec -u $DOCKER_USER $DOCKER_HOST       \
+             "`remote_bin_path $TC_SH`" $cmd         \
+             --prefix=$PREFIX                        \
+             --tomcat-version=$VER                   \
+             --start-port=$START_PORT                \
+             --stop-port=$STOP_PORT                  \
+             ${TC_OPTS}          
+      t=$?
       ;;
     *)
       tc="`local_bin_path $TC_SH`"
@@ -270,61 +261,138 @@ function control_tomcat() {
     echo -e "! control Tomcat =>[$cmd]  =failed"
   fi
   return $t
+}
 
-	#if [ 0 -eq $HAS_DOCKER ]; then
-	#	docker cp $TD_CTRL_SH $DOCKER_CONTAINER:$R_TD_CTRL_SH
-	#	docker exec $DOCKER_CONTAINER chown $DOCKER_USER:$DOCKER_USER $R_TD_CTRL_SH
-	#	case $PLATFORM in
-	#		MSYS_NT*)
-	#			./$TD_CTRL_BAT "$1"
-	#			;;
-	#		*)
-	#			docker exec -u $DOCKER_USER $DOCKER_CONTAINER $R_TD_CTRL_SH $1
-	#			;;
-	#	esac
-	#elif [ 0 -eq $HAS_SSH ]; then
-	#	ssh $SSH_USER@$SSH_HOST "$TC_SH_ENV bash $OPT_RUN/bin/$TC_SH $1"
-	#else
+function mkdir_remote() {
+  local d="$1"
+  local w="$2"
+  local t=
 
-                
-	#fi
+  echo -e "+ mkdir R[$d] ..."
+
+  case "$w" in
+    ssh)
+      ssh $SSH_USER@$SSH_HOST mkdir -p "$d"
+      t=$?
+      ;;
+    docker)
+      docker exec -u $DOCKER_USER $DOCKER_HOST mkdir -p "$d"
+      t=$?
+      ;;
+    *)
+      echo -e "# mkdir R[$d]: in local  =skipped"
+      t=0
+      ;;
+  esac
+
+  if [ 0 -eq $t ]; then
+    echo -e "+ mkdir R[$d]  =succeed"
+  else
+    echo -e "- mkdir R[$d]  =failed"
+  fi
+  return $t
+}
+
+function install_tomcat() {
+  local w="$1"
+  local tc=
+  local t=
+
+  echo -e "+ install Tomcat[$VER] ..."
+  if [ "${TO_WHERE[$TW_LOCAL]}" = "$w" ]; then
+    control_tomcat install "$w"
+    return $?
+  fi
+
+  tc="`local_bin_path $TC_SH`"
+  t=$?
+  if [ 0 -ne $t ]; then
+    echo -e "! install Tomcat[$VER]: $TC_SH no found  =failed"
+    return $t
+  fi
+
+  local tgz="apache-tomcat-$VER.tar.gz"
+  local ltgz=("`remote_root_path`/$tgz" "/tmp/$tgz" "./$tgz")
+  local rtgz="`remote_ver_path`/$tgz"
+
+  for f in "${ltgz[@]}"; do
+    if [ -f "$f" ]; then
+      ltgz="$f"
+      break;
+    fi
+  done
+
+  if [ ! -f "${ltgz[0]}" ]; then
+    $tc install --download-only --tomcat-version="$VER" --prefix="`dirname ${ltgz[0]}`"
+    t=$?
+    [ 0 -eq $t ] || return $t
+  fi
+
+  mkdir_remote "`remote_ver_path`/bin" "$w"
+  t=$?
+  [ 0 -eq $t ] || return $t
+
+  is_samed_file "${ltgz[0]}" "$rtgz"
+  t=$?
+  if [ 0 -ne $t ]; then
+    transport_file "${ltgz[0]}" "$rtgz" "$w"
+    t=$?
+    [ 0 -eq $t ] || return $t
+  fi
+
+  control_tomcat install "$w"
 }
 
 function is_samed_file() {
-  local lwp="$1"
-  local rwp="$2"
+  local lp="$1"
+  local rp="$2"
   local w="$3"
 
-  echo -e "? L[$lwp] samed with R[$rwp] ..."
-  if [ ! -f "$lwp" ]; then
+  echo -e "? L[$lp] samed with R[$rp] ..."
+  if [ ! -f "$lp" ]; then
     echo -e "! L[$1] does not exist."
     return 1
   fi
 
-  local lwh="`sha1sum $lwp | cut -d' ' -f1`"
-  local rwh="`test -f "$rwp" && sha1sum "$rwp" | cut -d' ' -f1`"
+  local lh="`sha1sum $lp | cut -d' ' -f1`"
+  local rh="`test -f "$rp" && sha1sum "$rp" | cut -d' ' -f1`"
 
   case "$w" in
     ssh)
-		  rwh=`ssh $SSH_USER@$SSH_HOST "test -f $rwp && sha1sum $rwp | cut -d' ' -f1"`
+		  rh=`ssh $SSH_USER@$SSH_HOST "test -f $rp && sha1sum $rp | cut -d' ' -f1"`
       ;;
     docker)
 		  case "$PLATFORM" in
 			  MSYS_NT*)
-				  rwh=`./$TD_SHA1SUM_BAT`
+				  rh=`./$TD_SHA1SUM_BAT`
 				  ;;
 			  *)
-				  rwh=`docker exec -u $DOCKER_USER $DOCKER_CONTAINER $R_TD_SHA1SUM_SH`
+          echo -e "+ generate D[$TD_SHA1SUM_SH] ..."
+          gen_docker_sha1sum_sh
+          t=$?
+          if [ 0 -ne $t ]; then
+            echo -e "! generate D[$TD_SHA1SUM_SH]  =failed"
+            return $t
+          else
+            echo -e "# generate D[$TD_SHA1SUM_SH]  =succeed"
+          fi
+          
+          local rbp="`remote_bin_path $TD_SHA1SUM_SH`"
+          transport_file "$TD_SHA1SUM_SH" "$rbp" "$w"
+          t=$?
+          [ 0 -eq $t ] || return $t
+
+				  rh=`docker exec -u $DOCKER_USER $DOCKER_HOST $rbp $rp`
 				  ;;
 		  esac       
       ;;
   esac
 
-  if [ "$lwh" = "$rwh" ]; then
-    echo -e "# L[$lwp] samed with R[$rwp]  =true"
+  if [ "$lh" = "$rh" ]; then
+    echo -e "# L[$lp] samed with R[$rp]  =true"
     return 0
   else
-    echo -e "! L[$lwp] samed with R[$rwp]  =false"
+    echo -e "! L[$lp] samed with R[$rp]  =false"
     return 1
   fi
 }
@@ -343,11 +411,12 @@ function build_war() {
       ;;
   esac
   if [ ! -d "$BUILD_DIR" ]; then
-    echo -e "\t ! L[$BUILD_DIR] does not exists."
+    echo -e "! build L[$lwp]: L[$BUILD_DIR] non-exists  =failed"
     return 1
   fi
 
   if [ ! -x "$cmd" ]; then
+    echo -e "! build L[$lwp]: build command no found  =failed"
     return 1
   fi
 
@@ -378,7 +447,8 @@ function transport_file() {
       t=$?
       ;;
     docker)
-      
+      docker cp $lp $DOCKER_HOST:$rp
+      t=$?
       ;;
     *)
 	    cp $lp $rp
@@ -391,44 +461,6 @@ function transport_file() {
     echo -e "! transport L[$lp] to R[$rp]  =failed"
   fi
   return $t
-    
-	#if [ 0 -eq $HAS_DOCKER ]; then
-	#	case $PLATFORM in
-	#		MSYS_NT*)
-	#			./$TD_RM_BAT
-	#			;;
-	#		*)
-	#			docker exec -u $DOCKER_USER $DOCKER_CONTAINER \
-	#				rm -rf ${WAR_DIR}${WAR_NAME} $R_WAR_FILE
-	#			;;
-	#	esac
-  #
-	#	docker cp $L_WAR_FILE $DOCKER_CONTAINER:$R_WAR_FILE 
-	#	docker exec $DOCKER_CONTAINER chown $DOCKER_USER:$DOCK<ER_USER $R_WAR_FILE
-	#	#docker_chown $R_WAR_FILE
-  #
-	#elif [ 0 -eq $HAS_SSH ]; then
-	#	ssh $SSH_USER@$SSH_HOST \
-	#		"$TC_SH_ENV rm -rf ${WAR_DIR}${WAR_NAME} $R_WAR_FILE"
-	#	scp $L_WAR_FILE $SSH_USER@$SSH_HOST:$R_WAR_FILE
-	#else
-	#	rm -rf ${WAR_DIR}${WAR_NAME} $R_WAR_FILE	
-	#	cp $L_WAR_FILE $R_WAR_FILE
-	#fi
-}
-
-control_mark() {
-  case "$TO_WHERE" in
-    ssh)
-		  echo -n "S[$SSH_USER@$SSH_HOST]"
-      ;;
-    docker)
-		  echo -n "D[$DOCKER_USER@$DOCKER_CONTAINER]"
-      ;;
-    *)
-		  echo -n "L[$PREFIX]"
-      ;;
-  esac
 }
 
 
@@ -456,6 +488,8 @@ do
     --local-war-path=*)      L_WAR_PATH="$value"	      ;;
     --ssh-user=*)            SSH_USER="$value"	        ;;
     --ssh-host=*)            SSH_HOST="$value"	        ;;
+    --docker-user=*)         DOCKER_USER="$value"       ;;
+    --docker-host=*)         DOCKER_HOST="$value"	      ;;
 
     --build)                 BUILD=yes    		          ;;
     --build-dir=*)           build_dir="$value"	        ;;
@@ -534,7 +568,7 @@ case "$command" in
     ;;
   start)
     if [ -z "$L_WAR_PATH" ]; then
-      echo -e "! missing --build-* options."
+      echo -e "! missing --local-war-path=* options."
       usage
       exit 1
     fi
@@ -542,6 +576,14 @@ case "$command" in
     retval=$?
     if [ "yes" = "$BUILD" -o 0 -ne $retval ]; then
       build_war "$L_WAR_PATH"
+      retval=$?
+      [ 0 -eq $retval ] || exit $retval
+    fi
+
+    check_exist "${TO_WHERE[$TW_IDX]}"
+    retval=$?
+    if [ 0 -ne $retval ]; then
+      install_tomcat "${TO_WHERE[$TW_IDX]}"
       retval=$?
       [ 0 -eq $retval ] || exit $retval
     fi
@@ -573,6 +615,10 @@ case "$command" in
     ;;
   check-pid)
     control_tomcat check-pid "${TO_WHERE[$TW_IDX]}"
+    exit $?
+    ;;
+  check-exist)
+    check_exist "${TO_WHERE[$TW_IDX]}"
     exit $?
     ;;
   *)
