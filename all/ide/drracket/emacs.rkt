20cf427f6dff25ea341beea5091a84ee5f5cec5e
南山竹 <junjiemars@gmail.com>
2017-08-09 23:36:43 +0800

    treat alt as meta

--- all/ide/drracket/emacs.rkt
+++ all/ide/drracket/emacs.rkt
@@ -2,7 +2,8 @@
 
 #|
 Emacs:
-* uncheck the __Enable keybindings__ in menus preference.
+* uncheck the __Enable keybindings__ in menus Preferences>Editing>General Editing.
+* check the __Treat alt as meta__ in Preferences>Editing>General Editing.
 * Edit>Keybindings>Add User-defined Keybindings...
 |#
 
