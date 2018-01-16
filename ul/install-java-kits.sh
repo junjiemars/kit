d9527ab2eb2695d68ec88d86746b40c907994f45
南山竹 <junjiemars@gmail.com>
2018-01-16 17:28:57 +0800

    what line 625 issue

--- ul/install-java-kits.sh
+++ ul/install-java-kits.sh
@@ -37,7 +37,7 @@ SOCKS="${SOCKS}"
 GITHUB_SH="https://raw.githubusercontent.com/junjiemars/kit/master/ul/install-java-kits.sh"
 APACHE_DIST="https://archive.apache.org/dist"
 
-HAS_ALL=${HAS_ALL:-NO}
+HAS_ALL="${HAS_ALL:-NO}"
 HAS_JDK=${HAS_JDK:-0}
 HAS_ABCL=${HAS_ABCL:-0}
 HAS_ANT=${HAS_ANT:-0}
@@ -51,7 +51,7 @@ HAS_CLOJURESCRIPT=${HAS_CLOJURESCRIPT:-0}
 HAS_GROOVY=${HAS_GROOVY:-0}
 HAS_SCALA=${HAS_SCALA:-0}
 HAS_SCALA_VIM=${HAS_SCALA_VIM:-0}
-HAS_STORM=${HAS_STORM:-0}
+# HAS_STORM=${HAS_STORM:-0}
 
 JDK_VER=("${JDK_U:-8u121} ${JDK_B:-b13}")
 ABCL_VER="${ABCL_VER:-1.5.0}"
@@ -63,7 +63,7 @@ CLOJURE_VER="${CLOJURE_VER:-1.8.0}"
 CLOJURESCRIPT_VER="${CLOJURESCRIPT_VER:-1.9.946}"
 GROOVY_VER="${GROOVY_VER:-2.4.12}"
 SCALA_VER="${SCALA_VER:-2.12.4}"
-STORM_VER="${STORM_VER:-1.1.1}"
+# STORM_VER="${STORM_VER:-1.1.1}"
 # ZOOKEEPER_VER="${ZOOKEEPER:-3.4.10}"
 
 declare -a KITS=()
@@ -539,30 +539,30 @@ install_scala() {
   return 1
 }
 
-install_storm() {
-  local storm_tgz="apache-storm-${STORM_VER}.tar.gz"
-  local storm_url="https://archive.apache.org/dist/storm/apache-storm-${STORM_VER}/${storm_tgz}"
-  local storm_home="${OPEN_DIR}/storm"
-  local bin_dir="${storm_home}/${STORM_VER}"
-  local cmd="${bin_dir}/bin/storm version"
-  local storm_sh="${RUN_DIR}/bin/storm"
+# install_storm() {
+#   local storm_tgz="apache-storm-${STORM_VER}.tar.gz"
+#   local storm_url="https://archive.apache.org/dist/storm/apache-storm-${STORM_VER}/${storm_tgz}"
+#   local storm_home="${OPEN_DIR}/storm"
+#   local bin_dir="${storm_home}/${STORM_VER}"
+#   local cmd="${bin_dir}/bin/storm version"
+#   local storm_sh="${RUN_DIR}/bin/storm"
 
-  `check_kit "storm version" "${storm_home}"` && return 0
+#   `check_kit "storm version" "${storm_home}"` && return 0
 
-  install_kit "${bin_dir}/bin/storm" \
-              "${cmd}" \
-              "${storm_url}" \
-              "${storm_home}/${storm_tgz}" \
-              "${bin_dir}"
-  [ 0 -eq $? ] || return 1
+#   install_kit "${bin_dir}/bin/storm" \
+#               "${cmd}" \
+#               "${storm_url}" \
+#               "${storm_home}/${storm_tgz}" \
+#               "${bin_dir}"
+#   [ 0 -eq $? ] || return 1
   
-  if `${cmd} &>/dev/null`; then
-    append_vars "STORM_HOME" "${bin_dir}"
-    append_paths "\${STORM_HOME}/bin" "STORM_HOME"
-    return 0
-  fi
-  return 1
-}
+#   if `${cmd} &>/dev/null`; then
+#     append_vars "STORM_HOME" "${bin_dir}"
+#     append_paths "\${STORM_HOME}/bin" "STORM_HOME"
+#     return 0
+#   fi
+#   return 1
+# }
 
 # install_zookeeper() {
 #   local zookeeper_tgz="zookeeper-${ZOOKEEPER_VER}.tar.gz"
@@ -604,7 +604,7 @@ if [ "YES" = "${HAS_ALL}" ]; then
   HAS_CLOJURESCRIPT=1
   HAS_GROOVY=1
   HAS_SCALA=1
-  HAS_STORM=1
+  # HAS_STORM=1
   # HAS_ZOOKEEPER=1
 fi
 
@@ -621,7 +621,7 @@ fi
 [ 0 -lt ${HAS_GROOVY} ]         && KITS+=('install_groovy')
 [ 0 -lt ${HAS_SCALA} ]          && KITS+=('install_scala')
 [ 0 -lt ${HAS_SCALA_VIM} ]      && KITS+=('install_scala_vim')
-[ 0 -lt ${HAS_STORM} ]          && KITS+=('install_storm')
+# [ 0 -lt ${HAS_STORM} ]          && KITS+=('install_storm')
 # [ 0 -lt ${HAS_ZOOKEEPER} ]      && KITS+=('install_zookeeper')
 
 
