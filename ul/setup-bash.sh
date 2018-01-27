0f6ef77a6e27032e877fe815c1baebfb3df26b59
南山竹 <junjiemars@gmail.com>
2018-01-27 16:11:06 +0800

    refine gen_dot_bash_logout fn

--- ul/setup-bash.sh
+++ ul/setup-bash.sh
@@ -135,7 +135,7 @@ gen_dot_bash_logout() {
 # when leaving the console clear the screen to increase privacy
 
 if [ "$SHLVL" = 1 ]; then
-    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+  [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
 fi
 
 END
