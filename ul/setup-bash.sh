ad6e9b1439bdcb15e34b45e02fe8dacd7345e41f
南山竹 <junjiemars@gmail.com>
2018-01-28 12:26:25 +0800

    fix unquoted var

--- ul/setup-bash.sh
+++ ul/setup-bash.sh
@@ -134,7 +134,7 @@ gen_dot_bash_logout() {
 # ~/.bash_logout: executed by bash(1) when login shell exits.
 # when leaving the console clear the screen to increase privacy
 
-if [ "$SHLVL" = 1 ]; then
+if [ "\$SHLVL" = 1 ]; then
   [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
 fi
 
