6cef21f36676299d529d40c1a288e1d9e6af9691
南山竹 <junjiemars@gmail.com>
2019-05-21 14:15:12 +0800

    refine nd.sh's upgrade command
    ND_ROOT and ND_PWD

--- ul/nd.sh
+++ ul/nd.sh
@@ -8,12 +8,13 @@
 
 VERSION="0.1.1"
 
-ROOT="${ROOT:-$(cd `dirname ${BASH_SOURCE[0]}`; pwd -P)}"
-OPT_RUN="${OPT_RUN:-${ROOT%/}/run}"
+ND_ROOT="${ND_ROOT:-$(cd `dirname ${BASH_SOURCE[0]}`; pwd -P)}"
+ND_PWD="`pwd -P`"
 
 find_nginx_home() {
-	for d in `ls -d ${ROOT%/}/*/`; do
-		if [[ $d =~ ${ROOT%/}/(nginx|nginx-release-*) ]]; then
+	local x="$@"
+	for d in `ls -d ${x%/}/*/`; do
+		if [[ $d =~ ${x%/}/(nginx|nginx-release-*) ]]; then
 			if [ -f "${d%/}/auto/configure" ]; then
 				echo "${d%/}"
 				return 0
@@ -25,19 +26,20 @@ find_nginx_home() {
 
 NGX_TARGET=( raw http https stream dns )
 NGX_IDX=( ${NGX_TARGET[0]} )
-NGX_HOME=${NGX_HOME:-`find_nginx_home`}
-NGX_RUN_DIR=${NGX_RUN_DIR:-$OPT_RUN}
-NGX_CONF_DIR=${NGX_CONF_DIR:-${NGX_RUN_DIR%/}/conf}
-NGX_LOG_DIR=${NGX_LOG_DIR:-${NGX_RUN_DIR%/}/var/nginx}
-NGX_OPTIONS=${NGX_OPTIONS}
+NGX_OPTIONS="${NGX_OPTIONS}"
+
+NGX_HOME="${NGX_HOME:-`find_nginx_home ${ND_PWD}`}"
+NGX_PREFIX="${NGX_PREFIX:-${PWD}/run}"
+NGX_CONF_DIR="${NGX_PREFIX%/}/conf"
+NGX_LOG_DIR="${NGX_LOG_DIR:-${NGX_PREFIX%/}/var/nginx}"
 
 NGX_ERR_LOG=error.log
 NGX_PID_LOG=pid
 
 NGX_CHAINED=( no yes )
 
-NGX_GEN_CONF=( yes no )
-NGX_GEN_SHELL=( yes no )
+NGX_GEN_CONF=( no yes )
+NGX_GEN_SHELL=( no yes )
 
 NGX_CONF="nginx.conf"
 NGX_SHELL="nginx.sh"
@@ -82,7 +84,7 @@ usage() {
   echo -e "  --options=  nginx auto/configure options, NGX_OPTIONS='${NGX_OPTIONS}'"
 	echo -e ""
   echo -e "  --home=      nginx source dir, NGX_HOME='${NGX_HOME}'"
-  echo -e "  --run-dir=   where nginx run, NGX_RUN_DIR='${NGX_RUN_DIR}'"
+  echo -e "  --prefix=    nginx prefix dir, NGX_PREFIX='${NGX_PREFIX}'"
   echo -e "  --conf-dir=  where nginx conf, NGX_CONF_DIR='${NGX_CONF_DIR}'"
   echo -e "  --log-dir=   where nginx log store, NGX_LOG_DIR='${NGX_LOG_DIR}'"
 	echo -e ""
@@ -124,11 +126,12 @@ do
     --version)                        version=yes                          ;;
 
     --target=*)                       ngx_target=( $value )                ;;
+    --options=*)                      ngx_options="$value"                 ;;
+
     --home=*)                         ngx_home="$value"                    ;;
-    --run-dir=*)                      ngx_run_dir="$value"                 ;;
+    --prefix=*)                       ngx_prefix="$value"                  ;;
     --conf-dir=*)                     ngx_conf_dir="$value"                ;;
     --log-dir=*)                      ngx_log_dir="$value"                 ;;
-    --options=*)                      ngx_options="$value"                 ;;
 
     --chained=*)                      ngx_chained="$value"                 ;;
     --gen-conf=*)                     ngx_gen_conf="$value"                ;;
@@ -173,26 +176,25 @@ fi
 retval=0
 
 if [ -n "$ngx_home" ]; then
-	NGX_HOME="$ngx_home"
+	NGX_HOME="${ngx_home%/}"
 fi
 if [ ! -d "$NGX_HOME" ]; then
 	echo -e "! --home=$NGX_HOME  =invalid"
 	exit 1
 fi
 
-
-if [ -n "$ngx_run_dir" ]; then
-  NGX_RUN_DIR="$ngx_run_dir"
+if [ -n "$ngx_prefix" ]; then
+  NGX_PREFIX="${ngx_prefix%/}"
 fi
-if [ ! -d "$NGX_RUN_DIR" ]; then
-	echo -e "! --run-dir=$NGX_RUN_DIR  =invalid, try to create '$NGX_RUN_DIR' ..."
-	mkdir -p "$NGX_RUN_DIR"
+if [ ! -d "$NGX_PREFIX" ]; then
+	echo -e "! --prefix=$NGX_PREFIX  =invalid, try to create '$NGX_PREFIX' ..."
+	mkdir -p "$NGX_PREFIX"
 	retval=$?
 	[ 0 -eq $retval ] || exit $retval
 fi
 
 if [ -n "$ngx_conf_dir" ]; then
-	NGX_CONF_DIR="$ngx_conf_dir"
+	NGX_CONF_DIR="${ngx_conf_dir%/}"
 fi
 if [ ! -d "$NGX_CONF_DIR" ]; then
 	echo -e "! --conf-dir=$NGX_CONF_DIR  =invalid, try to create '$NGX_CONF_DIR'..."
@@ -202,7 +204,7 @@ if [ ! -d "$NGX_CONF_DIR" ]; then
 fi
 
 if [ -n "$ngx_log_dir" ]; then
-	NGX_LOG_DIR="$ngx_log_dir"
+	NGX_LOG_DIR="${ngx_log_dir%/}"
 fi
 if [ ! -d "$NGX_LOG_DIR" ]; then
 	echo -e "! --log-dir=$NGX_LOG_DIR  =invalid, try to create '$NGX_LOG_DIR' ..."
@@ -263,7 +265,7 @@ do_list() {
 
 configure_prefix() {
 echo "\
---prefix=$NGX_RUN_DIR                             \
+--prefix=$NGX_PREFIX                             \
 --error-log-path=${NGX_LOG_DIR%/}/$NGX_ERR_LOG    \
 --pid-path=${NGX_LOG_DIR%/}/$NGX_PID_LOG          \
 "
@@ -378,6 +380,12 @@ do_modules() {
 do_upgrade() {
 	local t=0
 
+	if [ "yes" = "$NGX_CHAINED" ]; then
+		do_configure	
+		t=$?
+		[ 0 -eq $t ] || exit $t
+	fi
+
 	cd $NGX_HOME
 	make upgrade
 }
@@ -430,7 +438,7 @@ http {
     #tcp_nopush     on;
 
     upstream backend {
-        #server x.x.x.x:8181 weight=5;
+        server 127.0.0.1:8181 weight=5;
         #server x.x.x.x:8282 backup;
 `
 for i in ${OPT_UPSTREAM[@]}; do
@@ -574,7 +582,7 @@ gen_shell() {
 # $0
 #    --target='${NGX_IDX[@]}'
 #    --home=$NGX_HOME
-#    --run-dir=$NGX_RUN_DIR
+#    --run-dir=$NGX_PREFIX
 #    --conf-dir=$NGX_CONF_DIR
 #    --log-dir=$NGX_LOG_DIR
 #    --options=$NGX_OPTIONS
@@ -588,14 +596,17 @@ gen_shell() {
 #    --opt-server-name=$OPT_SERVER_NAME
 #    --opt-server-tokens=$OPT_SERVER_TOKENS
 
-ROOT=${ROOT%/}
-NGX_HOME=${NGX_RUN_DIR%/}
-NGX_CONF_DIR=${NGX_CONF_DIR}
-NGX_BIN=\${NGX_HOME}/sbin/nginx
+ND_ROOT="${ND_ROOT%/}"
+ND_PWD="${ND_PWD%/}"
+NGX_PREFIX="${NGX_PREFIX%/}"
+NGX_CONF_DIR="${NGX_CONF_DIR%/}"
+NGX_LOG_DIR="${NGX_LOG_DIR%/}"
+NGX_BIN="${NGX_PREFIX%/}/sbin/nginx"
+NGX_PID="${NGX_LOG_DIR%/}/${NGX_PID_LOG}"
 
 check_nginx_env() {
-  if [ ! -d "\$NGX_HOME" ]; then
-    echo -e "! \$NGX_HOME does not exist"
+  if [ ! -d "\$NGX_PREFIX" ]; then
+    echo -e "! \$NGX_PREFIX does not exist"
     exit 1
   fi
 
@@ -611,7 +622,7 @@ check_nginx_env() {
 }
 
 copy_nginx_conf() {
-  local s="\${ROOT}/$NGX_CONF"
+  local s="\${ND_PWD}/$NGX_CONF"
   local d="\${NGX_CONF_DIR}/$NGX_CONF"
 
   if [ ! -f "\$s" ]; then
@@ -636,9 +647,22 @@ copy_nginx_conf() {
   cp "\${s}" "\${d}"
 }
 
-check_nginx_env
-copy_nginx_conf
+echo -n "+check nginx env ... "
+if \`check_nginx_env\`; then
+	echo "done"
+else
+	echo "failed"
+fi
 
+echo -n "+copy nginx conf ... "
+if \`copy_nginx_conf\`; then
+	echo "done"
+else
+	echo "failed"
+fi
+
+echo "+run: \$@ ... "
+echo "--------------------"
 \${NGX_BIN} \$@
 
 END
