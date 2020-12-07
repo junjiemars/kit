eb8902029394e14e1908e36090723d51270d9879
南山竹 <junjiemars@gmail.com>
2020-12-07 17:58:59 +0800

    snap remove old

---
+++ ul/snap-rm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+# LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+#   while read snapname revision; do
+#     sudo snap remove "$snapname" --revision="$revision"
+#   done
+
+
+snap list --all | while read snapname ver rev trk pub notes; do if [[ $notes = *disabled* ]]; then snap remove "$snapname" --revision="$rev"; fi; done
