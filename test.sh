6049d59ad3e6e4b78fc2a896a1559650e0eeb785
南山竹 <junjiemars@gmail.com>
2021-01-04 21:22:05 +0800

    test

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+_ROOT_DIR_="`cd -- $(dirname -- $0) && pwd`"
+_TEST_="${_TEST_:-basic}"
+_OS_NAME_="`uname -s 2>/dev/null`"
+
+case "$_OS_NAME_" in
+  MSYS_NT-*|MINGW??_NT-*) _OS_NAME_="WinNT" ;;
+esac
+
+# switch to ROOT
+cd "${_ROOT_DIR_}"
+
+test_do() {
+  echo "test $@ ..."
+  $@
+}
+
+# basic test
+if [ "basic" = "$_TEST_" ]; then
+  test_do ul/sh.sh       || exit 1
+fi
+
+# eof
